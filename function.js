67c8f1894d44e56caf696edd28bd545659950962
KicauID <Kicauid.official@gmail.com>
2024-07-01 10:48:07 +0700

    Update function.js

--- function.js
+++ function.js
@@ -5,11 +5,11 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
         standard: 1.5,
         high: 2,
     };
-    
+
     // DYNAMIC VALUES
     html = html.value ?? "No HTML set.";
     fileName = fileName.value ?? "file";
-    format = format.value ?? "a4";
+    format = format.value ?? "thermal_80mm"; // Changed format to thermal_80mm
     zoom = zoom.value ?? "1";
     orientation = orientation.value ?? "portrait";
     margin = margin.value ?? "0";
@@ -21,6 +21,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 
     // DOCUMENT DIMENSIONS
     const formatDimensions = {
+        // Dimensions for thermal_80mm
         thermal_80mm: [224, 1050],
     };
 
@@ -28,7 +29,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
     const dimensions = customDimensions || formatDimensions[format];
     const finalDimensions = dimensions.map((dimension) => Math.round(dimension / zoom));
 
-    // LOG SETTINGS TO CONSOLE (optional)
+    // LOG SETTINGS TO CONSOLE
     console.log(
         `Filename: ${fileName}\n` +
         `Format: ${format}\n` +
@@ -44,38 +45,139 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
     );
 
     const customCSS = `
-    /* Add your custom CSS styles here */
+    body {
+      margin: 0!important;
+    }
+
+    .button {
+      width: 50%;
+      border-radius: 0;
+      font-size: 14px;
+      font-weight: 600;
+      line-height: 1.5rem;
+      color: #ffffff;
+      border: none;
+      font-family: 'Arial';
+      padding: 0px 12px;
+      height: 32px;
+      text-transform: uppercase;
+      cursor: pointer;
+      box-shadow: 0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 1px 2.5px rgba(0, 0, 0, 0.1);
+      position: fixed;
+      top: 0;
+      z-index: 1000;
+    }
+
+    button#download {
+      background: #04A535;
+      left: 0;
+    }
+
+    button#print { /* Changed text to 'CETAK' */
+      background: #0353A7;
+      right: 0;
+    }
+
+    button#download:hover, button#print:hover {
+      background: #f5f5f5;
+      color: #000000;
+    }
+
+    button#download.downloading, button#print.printing {
+      background: #ffffff;
+      color: #000000;
+    }
+
+    button#download.done, button#print.done {
+      background: #ffffff;
+      color: #000000;
+    }
+
+    ::-webkit-scrollbar {
+      width: 5px;
+      background-color: rgb(0 0 0 / 8%);
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: rgb(0 0 0 / 32%);
+      border-radius: 4px;
+    }
     `;
 
     const originalHTML = `
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
     <style>${customCSS}</style>
     <div class="main">
-      <button class="button" id="print">Print</button>
+      <button class="button" id="download">Download</button>
+      <button class="button" id="print">CETAK</button> <!-- Changed text to 'CETAK' -->
       <div id="content" class="content">${html}</div>
     </div>
     <script>
-      document.getElementById('print').addEventListener('click', function() {
+      document.getElementById('download').addEventListener('click', function() {
+        var element = document.getElementById('content');
+        var button = this;
+        button.innerText = 'DOWNLOADING...';
+        button.className = 'downloading';
+
+        var opt = {
+          pagebreak: { mode: ['css'], before: ${JSON.stringify(breakBefore)}, after: ${JSON.stringify(breakAfter)}, avoid: ${JSON.stringify(breakAvoid)} },
+          margin: ${margin},
+          filename: '${fileName}',
+          html2canvas: {
+            useCORS: true,
+            scale: ${quality}
+          },
+          jsPDF: {
+            unit: 'px',
+            orientation: '${orientation}',
+            format: [${finalDimensions}],
+            hotfixes: ['px_scaling']
+          }
+        };
+        html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
+          button.innerText = 'DOWNLOAD DONE';
+          button.className = 'done';
+          setTimeout(function() { 
+            button.innerText = 'Download';
+            button.className = ''; 
+          }, 2000);
+        }).save();
+      });
+
+      document.getElementById('print').addEventListener('click', function() { /* Changed id to 'print' */
         var element = document.getElementById('content');
         var button = this;
         button.innerText = 'PRINTING...';
         button.className = 'printing';
 
-        // You can add specific printing logic here for Bluetooth thermal printer
-        // Ensure your Bluetooth printer is properly connected and configured
-
-        // Example:
-        window.print();
-
-        button.innerText = 'PRINT DONE';
-        button.className = 'done';
-        setTimeout(function() { 
-          button.innerText = 'Print';
-          button.className = ''; 
-        }, 2000);
+        var opt = {
+          pagebreak: { mode: ['css'], before: ${JSON.stringify(breakBefore)}, after: ${JSON.stringify(breakAfter)}, avoid: ${JSON.stringify(breakAvoid)} },
+          margin: ${margin},
+          filename: '${fileName}',
+          html2canvas: {
+            useCORS: true,
+            scale: ${quality}
+          },
+          jsPDF: {
+            unit: 'px',
+            orientation: '${orientation}',
+            format: [${finalDimensions}],
+            hotfixes: ['px_scaling']
+          }
+        };
+        html2pdf().set(opt).from(element).toPdf().get('pdf').then(function(pdf) {
+          pdf.autoPrint();
+          window.open(pdf.output('bloburl'), '_blank');
+          button.innerText = 'PRINT DONE';
+          button.className = 'done';
+          setTimeout(function() { 
+            button.innerText = 'CETAK'; /* Changed text to 'CETAK' */
+            button.className = ''; 
+          }, 2000);
+        });
       });
     </script>
     `;
-
     var encodedHtml = encodeURIComponent(originalHTML);
     return "data:text/html;charset=utf-8," + encodedHtml;
 };
